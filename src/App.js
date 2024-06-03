e5e82d62806caf895b082b089d59ea52dee119f4
Armaan Sawhney <sawhney7@students.cs.ubc.ca>
2024-06-03 16:24:00 +0530

    Fixed about

--- src/App.js
+++ src/App.js
@@ -7,62 +7,56 @@ function App() {
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
           <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
             <div className="flex flex-col grow justify-center w-full text-center bg-white max-md:max-w-full">
-              <div className="flex justify-center items-center px-16 py-12 text-4xl font-semibold text-blue-700 bg-neutral-900 max-md:px-5 max-md:max-w-full">
+              <div className="flex justify-center items-center px-16 py-12 text-4xl font-semibold text-cyan-700 bg-neutral-900 max-md:px-5 max-md:max-w-full">
                 <div className="flex gap-5">
                   <img
                     loading="lazy"
                     src="https://cdn.builder.io/api/v1/image/assets/TEMP/ff623c30a21da4ec87e462db1a01ca4190cc9454833f6afc23688b53bb034584?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&"
-                    className="shrink-0 aspect-square w-[50px]"
+                    className="shrink-0 aspect-square w-[50px] filter grayscale mt-2"
                   />
                   <div className="flex-auto self-start mt-3">
-                    Armaan Sawhneyy
+                    PORTFOLIO 
                     <br />
                   </div>
                 </div>
               </div>
               <div className="flex justify-center items-center px-16 py-20 font-bold bg-neutral-900 max-md:px-5 max-md:max-w-full">
                 <div className="flex flex-col items-center mt-28 mb-16 max-w-full w-[658px] max-md:my-10">
-                  <div className="text-4xl text-blue-700">
-                    <span className="text-4xl text-blue-700">
-                      Hello, I’m Armaan
+                  <div className="text-4xl text-cyan-700">
+                    <span className="text-4xl text-cyan-700">
+                      Hello!, I’m Armaan
                     </span>
                     <br />
-                    <span className="text-5xl text-blue-700">
-                      ABCXYZ Position
+                    <span className="text-5xl text-cyan-700">
+                      I'm an upcoming Software Engineer!
                     </span>
                   </div>
                   <div className="self-stretch mt-20 text-2xl text-neutral-200 max-md:mt-10 max-md:max-w-full">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Maxime consequatur unde sint expedita, repellat temporibus
-                    asperiores eveniet eaque a dignissimos! Natus dicta
-                    sapiente, cum incidunt odit voluptates veniam aspernatur
-                    nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing
-                    elit. Maxime consequatur unde sint expedita, repellat
-                    temporibus asperiores eveniet eaque a dignissimos! Natus
-                    dicta sapiente, cum incidunt odit voluptates veniam
-                    aspernatur nostrum!
+                    I'm a third-year student at The University of British Columbia, Vancouver, specializing in Artificial Intelligence (AI) and Machine Learning. 
+                    Through my academic coursework, I have developed a strong foundation in software development, excelling in full-stack development technologies. 
+                    Checkout my LinkedIn below! 
                   </div>
-                  <div className="justify-center items-center px-16 py-8 mt-16 max-w-full text-3xl bg-blue-700 rounded-[50px] text-zinc-100 text-opacity-90 w-[300px] max-md:px-5 max-md:mt-10">
-                    Contact Me
-                  </div>
+                  <button className="justify-center items-center px-16 py-8 mt-16 max-w-full text-3xl bg-cyan-700 rounded-[50px] text-zinc-100 text-opacity-90 w-[300px] max-md:px-5 max-md:mt-10">
+                    LinkedIn
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
             <div className="flex flex-col grow justify-center w-full bg-neutral-900 max-md:max-w-full">
-              <div className="flex justify-center items-center px-16 py-16 text-2xl font-semibold text-center text-blue-700 whitespace-nowrap bg-neutral-900 max-md:px-5 max-md:max-w-full">
+              <div className="flex justify-center items-center px-16 py-16 text-2xl font-semibold text-center text-cyan-700 whitespace-nowrap bg-neutral-900 max-md:px-5 max-md:max-w-full">
                 <div className="flex gap-5 justify-between max-w-full w-[472px] max-md:flex-wrap">
-                  <div>About</div>
-                  <div>Contact</div>
-                  <div>Projects</div>
+                  <button>About</button>
+                  <button>Education</button>
+                  <button>Projects</button>
                 </div>
               </div>
               <div className="flex justify-center items-center px-16 py-20 bg-neutral-900 max-md:px-5 max-md:max-w-full">
                 <img
                   loading="lazy"
                   srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/01f3dc932c8aaa7e7ad51aaba164d63f1009097f89e1ace2d53a239f281060e1?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/01f3dc932c8aaa7e7ad51aaba164d63f1009097f89e1ace2d53a239f281060e1?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/01f3dc932c8aaa7e7ad51aaba164d63f1009097f89e1ace2d53a239f281060e1?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/01f3dc932c8aaa7e7ad51aaba164d63f1009097f89e1ace2d53a239f281060e1?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/01f3dc932c8aaa7e7ad51aaba164d63f1009097f89e1ace2d53a239f281060e1?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/01f3dc932c8aaa7e7ad51aaba164d63f1009097f89e1ace2d53a239f281060e1?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/01f3dc932c8aaa7e7ad51aaba164d63f1009097f89e1ace2d53a239f281060e1?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/01f3dc932c8aaa7e7ad51aaba164d63f1009097f89e1ace2d53a239f281060e1?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&"
-                  className="mt-14 mb-20 max-w-full aspect-square w-[600px] max-md:my-10"
+                  className="mt-14 mb-20 max-w-full aspect-square w-[600px] max-md:my-10 filter grayscale"
                 />
               </div>
             </div>
@@ -74,19 +68,20 @@ function App() {
           <div className="flex gap-5 max-md:flex-col max-md:gap-0">
             <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
               <img
-                loading="lazy"
-                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/a0e9143540c3d127e5d2eb5c77edb0ca164788bfc431ecbc0fbf6a9ccb2a4006?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/a0e9143540c3d127e5d2eb5c77edb0ca164788bfc431ecbc0fbf6a9ccb2a4006?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/a0e9143540c3d127e5d2eb5c77edb0ca164788bfc431ecbc0fbf6a9ccb2a4006?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/a0e9143540c3d127e5d2eb5c77edb0ca164788bfc431ecbc0fbf6a9ccb2a4006?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/a0e9143540c3d127e5d2eb5c77edb0ca164788bfc431ecbc0fbf6a9ccb2a4006?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/a0e9143540c3d127e5d2eb5c77edb0ca164788bfc431ecbc0fbf6a9ccb2a4006?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/a0e9143540c3d127e5d2eb5c77edb0ca164788bfc431ecbc0fbf6a9ccb2a4006?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/a0e9143540c3d127e5d2eb5c77edb0ca164788bfc431ecbc0fbf6a9ccb2a4006?apiKey=a2ae5ec7e5544df495302e8dcdfc0e04&"
-                className="mt-48 w-full aspect-square max-md:mt-10 max-md:max-w-full"
+                loading="eager"
+                src="https://logowik.com/content/uploads/images/ubc-university-of-british-columbia3090.logowik.com.webp"
+                alt="Ubc Logos Download Mac Cosmetics Logo - Ubc Logo Png Clipart@pikpng.com"
+                className="mt-48 w-100 max-md:mt-10 max-md:max-w-full align-left"
               />
             </div>
-            <div className="flex flex-col ml-5 w-[67%] max-md:ml-0 max-md:w-full">
-              <div className="flex flex-col font-bold text-center max-md:mt-10 max-md:max-w-full">
-                <div className="text-5xl text-blue-700 max-md:max-w-full max-md:text-4xl">
-                  Get to Know Me
+            <div className="flex flex-col ml-7 w-[67%] max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col font-bold text-left max-md:mt-10 max-md:max-w-full">
+                <div className="text-5xl text-cyan-700 max-md:max-w-full max-md:text-4xl">
+                  About My Education
                 </div>
                 <div className="flex flex-col self-end mt-44 max-w-full w-[676px] max-md:mt-10">
                   <div className="text-2xl text-neutral-200 max-md:max-w-full">
-                    <span className="text-5xl text-blue-700">Lorem</span> ipsum
+                    <span className="text-5xl text-cyan-700 text-center">Lorem</span> ipsum
                     dolor sit amet consectetur, adipisicing elit. Maxime
                     consequatur unde sint expedita, repellat temporibus
                     asperiores eveniet eaque a dignissimos! Natus dicta
@@ -98,7 +93,7 @@ function App() {
                     consectetur, adipisicing elit. Maxime consequatur unde sint
                     expedita, repellat temporibus asperiores eveniet
                   </div>
-                  <div className="justify-center items-center self-center px-16 py-8 mt-20 max-w-full text-3xl bg-blue-700 rounded-[50px] text-zinc-100 text-opacity-90 w-[300px] max-md:px-5 max-md:mt-10">
+                  <div className="justify-center items-center self-center px-16 py-8 mt-20 max-w-full text-3xl bg-cyan-700 rounded-[50px] text-zinc-100 text-opacity-90 w-[300px] max-md:px-5 max-md:mt-10">
                     Contact Me
                   </div>
                 </div>
@@ -109,7 +104,7 @@ function App() {
       </div>
       <div className="flex justify-center items-center px-16 py-20 w-full bg-neutral-900 max-md:px-5 max-md:max-w-full">
         <div className="flex flex-col w-full max-w-[1694px] max-md:max-w-full">
-          <div className="self-center text-5xl font-bold text-center text-blue-700 max-md:max-w-full max-md:text-4xl">
+          <div className="self-center text-5xl font-bold text-center text-cyan-700 max-md:max-w-full max-md:text-4xl">
             Some of my Projects
           </div>
           <div className="mt-16 max-md:mt-10 max-md:max-w-full">
@@ -129,7 +124,7 @@ function App() {
                     consequatur unde sint expedita, repellat temporibus
                     asperiores eveniet eaque a dignissimos!{" "}
                   </div>
-                  <div className="justify-center items-center px-16 py-7 mt-7 text-3xl text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:max-w-full">
+                  <div className="justify-center items-center px-16 py-7 mt-7 text-3xl text-white whitespace-nowrap bg-cyan-700 rounded-[30px] max-md:px-5 max-md:max-w-full">
                     View
                   </div>
                 </div>
@@ -149,9 +144,9 @@ function App() {
                     consequatur unde sint expedita, repellat temporibus
                     asperiores eveniet eaque a dignissimos!{" "}
                   </div>
-                  <div className="justify-center items-center px-16 py-7 mt-7 text-3xl text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:max-w-full">
+                  <button className="justify-center items-center px-16 py-7 mt-7 text-3xl text-white whitespace-nowrap bg-cyan-700 rounded-[30px] max-md:px-5 max-md:max-w-full">
                     View
-                  </div>
+                  </button>
                 </div>
               </div>
               <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
@@ -169,7 +164,7 @@ function App() {
                     consequatur unde sint expedita, repellat temporibus
                     asperiores eveniet eaque a dignissimos!{" "}
                   </div>
-                  <div className="justify-center items-center px-16 py-7 mt-7 text-3xl text-white whitespace-nowrap bg-blue-700 rounded-[30px] max-md:px-5 max-md:max-w-full">
+                  <div className="justify-center items-center px-16 py-7 mt-7 text-3xl text-white whitespace-nowrap bg-cyan-700 rounded-[30px] max-md:px-5 max-md:max-w-full">
                     View
                   </div>
                 </div>
@@ -180,14 +175,14 @@ function App() {
       </div>
       <div className="flex justify-center items-center px-16 py-20 w-full bg-neutral-900 max-md:px-5 max-md:max-w-full">
         <div className="flex flex-col mt-5 w-full max-w-[1633px] max-md:max-w-full">
-          <div className="self-center text-5xl font-bold text-center text-blue-700 max-md:max-w-full max-md:text-4xl">
+          <div className="self-center text-5xl font-bold text-center text-cyan-700 max-md:max-w-full max-md:text-4xl">
             My Contact Information
           </div>
           <div className="mt-20 max-md:mt-10 max-md:max-w-full">
             <div className="flex gap-5 max-md:flex-col max-md:gap-0">
               <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
                 <div className="flex flex-col grow px-20 py-20 mt-3 w-full text-2xl font-bold text-center whitespace-nowrap bg-neutral-900 bg-opacity-10 text-zinc-100 max-md:px-5 max-md:mt-10 max-md:max-w-full">
-                  <div className="self-center text-3xl text-blue-700">
+                  <div className="self-center text-3xl text-cyan-700">
                     E-Mail
                   </div>
                   <div className="mt-14 max-md:mt-10">
@@ -201,7 +196,7 @@ function App() {
               <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
                 <div className="flex justify-center items-center px-16 py-20 w-full text-2xl font-bold text-center bg-neutral-900 bg-opacity-10 text-zinc-100 max-md:px-5 max-md:mt-10 max-md:max-w-full">
                   <div className="flex flex-col">
-                    <div className="self-center text-3xl text-blue-700">
+                    <div className="self-center text-3xl text-cyan-700">
                       Phone
                     </div>
                     <div className="mt-14 max-md:mt-10">+91 555-6666-444</div>
@@ -211,7 +206,7 @@ function App() {
               </div>
               <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
                 <div className="flex flex-col grow px-20 py-20 mt-3 w-full text-2xl font-bold text-center whitespace-nowrap bg-neutral-900 bg-opacity-10 text-zinc-100 max-md:px-5 max-md:mt-10 max-md:max-w-full">
-                  <div className="self-center text-3xl text-blue-700">
+                  <div className="self-center text-3xl text-cyan-700">
                     E-Mail
                   </div>
                   <div className="mt-14 max-md:mt-10">
